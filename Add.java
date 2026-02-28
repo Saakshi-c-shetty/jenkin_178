5b03224094d608f523a55dc1a895771c9288663c
Saakshi-c-shetty <shettysaakshic@gmail.com>
2026-02-28 11:54:41 +0530

    Create Add.java

---
+++ Add.java
@@ -0,0 +1,11 @@
+public class Add {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        int a = 5;
+        int b = 10;
+        int sum = a + b;
+
+        System.out.println("Sum of " + a + " and " + b + " is: " + sum);
+    }
+}
